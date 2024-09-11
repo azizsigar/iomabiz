615a7d5ffe01b61add37148d1a9911744c79b62a
Aziz <103178186+azizsigar@users.noreply.github.com>
2024-09-11 09:24:01 +0200

    finisehd cv site

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,12 +3,22 @@ export default function Main() {
   return (
     <div className="main">
       <div className="main-content">
-        <h1>Where Creativity Meets Code.</h1>
+        <h1 class="slide-in">Where Creativity Meets Code.</h1>
         <p>
           As a junior developer venturing into design, I am dedicated to
           blending creativity with technical expertise. Explore how my journey
           in design and development brings innovative ideas to life and
           transforms concepts into impactful digital solutions.
+          <br />
+          <br />
+          <a
+            href="https://www.linkedin.com/in/aziz-sigar-5009611b2/?originalSubdomain=nl"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="contact-link"
+          >
+            <strong>Click here to get in touch</strong>
+          </a>
         </p>
       </div>
     </div>
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,27 +1,54 @@
 import "../works.css";
+
 export default function Works() {
   return (
     <div className="works-parent">
-      <h1>Works</h1>
-      <a
-        href="https://imtiaz101325.github.io/hyf-c48-w2-browsers-quiz-app-coffee-break/"
-        target="blank"
-      >
-        coffiebreak
-      </a>
-      <a
-        href="https://azizsigar.github.io/personal-recipe-project/"
-        target="blank"
-      >
-        recipe sites
-      </a>
-      <a
-        href="https://azizsigar.githubhttps://azizsigar.github.io/randomquickmath/.io/azizsigar.github.io/"
-        target="blank"
-      >
-        random quick math
-      </a>
-      <a href="https://www.youtube.com/playlist?list=PLx1wRpHvwJABzF2BOSbtf8-fBJXQriqc0">video editing</a>
+      <h3>Click to go</h3>
+      <div className="work-childs">
+        <div className="work-item">
+          <a
+            href="https://imtiaz101325.github.io/hyf-c48-w2-browsers-quiz-app-coffee-break/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coffee Break have a quiz
+          </a>
+            <video src="/media/work4.mp4" autoPlay loop muted playsInline></video>
+        </div>
+
+        <div className="work-item">
+          <a
+            href="https://azizsigar.github.io/personal-recipe-project/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Recipe from the world
+          </a>
+          <video src="/media/work1.mp4" autoPlay loop muted playsInline></video>
+        </div>
+
+        <div className="work-item">
+          <a
+            href="https://azizsigar.github.io/randomquickmath/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Random Quick Math Game
+          </a>
+          <video src="/media/work3.mp4" autoPlay loop muted playsInline></video>
+        </div>
+
+        <div className="work-item">
+          <a
+            href="https://www.youtube.com/playlist?list=PLx1wRpHvwJABzF2BOSbtf8-fBJXQriqc0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Video Editing Projects
+          </a>
+<video src="/media/work2.mp4" autoPlay loop muted playsInline></video>
+        </div>
+      </div>
     </div>
   );
 }
